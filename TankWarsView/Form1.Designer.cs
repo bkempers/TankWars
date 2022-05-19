b76b15fa2c741e54d127c043d56fc562e1132c45
Ben Kempers <77750841+bkempers@users.noreply.github.com>
2022-05-19 17:16:07 -0600

    Add files via upload

---
+++ TankWarsView/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace GameView
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.IPAddressTextBox = new System.Windows.Forms.ToolStripTextBox();
+            this.IPAddressLabel = new System.Windows.Forms.Label();
+            this.NameTextBox = new System.Windows.Forms.TextBox();
+            this.NameLabel = new System.Windows.Forms.Label();
+            this.ConnectButton = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(25, 25);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.IPAddressTextBox});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(817, 27);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "MenuStrip";
+            // 
+            // IPAddressTextBox
+            // 
+            this.IPAddressTextBox.Name = "IPAddressTextBox";
+            this.IPAddressTextBox.Size = new System.Drawing.Size(100, 23);
+            this.IPAddressTextBox.Text = "localhost";
+            // 
+            // IPAddressLabel
+            // 
+            this.IPAddressLabel.AutoSize = true;
+            this.IPAddressLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IPAddressLabel.Location = new System.Drawing.Point(108, 6);
+            this.IPAddressLabel.Name = "IPAddressLabel";
+            this.IPAddressLabel.Size = new System.Drawing.Size(64, 13);
+            this.IPAddressLabel.TabIndex = 1;
+            this.IPAddressLabel.Text = "IPAddress";
+            // 
+            // NameTextBox
+            // 
+            this.NameTextBox.Location = new System.Drawing.Point(202, 3);
+            this.NameTextBox.Name = "NameTextBox";
+            this.NameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.NameTextBox.TabIndex = 2;
+            this.NameTextBox.Text = "player";
+            // 
+            // NameLabel
+            // 
+            this.NameLabel.AutoSize = true;
+            this.NameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NameLabel.Location = new System.Drawing.Point(302, 6);
+            this.NameLabel.Name = "NameLabel";
+            this.NameLabel.Size = new System.Drawing.Size(78, 13);
+            this.NameLabel.TabIndex = 3;
+            this.NameLabel.Text = "Player Name";
+            // 
+            // ConnectButton
+            // 
+            this.ConnectButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ConnectButton.Location = new System.Drawing.Point(409, 1);
+            this.ConnectButton.Name = "ConnectButton";
+            this.ConnectButton.Size = new System.Drawing.Size(75, 23);
+            this.ConnectButton.TabIndex = 4;
+            this.ConnectButton.Text = "Connect";
+            this.ConnectButton.UseMnemonic = false;
+            this.ConnectButton.UseVisualStyleBackColor = true;
+            this.ConnectButton.Click += new System.EventHandler(this.ConnectButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(817, 497);
+            this.Controls.Add(this.ConnectButton);
+            this.Controls.Add(this.NameLabel);
+            this.Controls.Add(this.NameTextBox);
+            this.Controls.Add(this.IPAddressLabel);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "TankWars";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripTextBox IPAddressTextBox;
+        private System.Windows.Forms.Label IPAddressLabel;
+        private System.Windows.Forms.TextBox NameTextBox;
+        private System.Windows.Forms.Label NameLabel;
+        private System.Windows.Forms.Button ConnectButton;
+    }
+}
+
---
+++ TankWarsView/Form1.cs
@@ -0,0 +1,143 @@
+﻿using GameController;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GameModel;
+using System.Windows.Forms.DataVisualization.Charting;
+
+/// <summary>
+/// Authors: Anish Narayanswamy and Ben Kempers
+/// 
+/// This class contains the View of the MVC, responsible for displaying what has been drawn from the DrawingPanel.
+/// </summary>
+namespace GameView
+{
+    public partial class Form1 : Form
+    {
+        private gameController controller;
+        private World world;
+        private DrawingPanel drawingPanel;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            controller = new gameController();
+            world = controller.GetWorld();
+            controller.UpdateArrived += OnFrame;
+
+            ClientSize = new Size(900, 925);
+            
+            //Drawing panel instanciation into the Form
+            drawingPanel = new DrawingPanel(world);
+            drawingPanel.Location = new Point(0, 25);
+            drawingPanel.Size = new Size(900, 925);
+            drawingPanel.BackColor = Color.Black;
+                
+            //Key and mouse event handlers
+            this.KeyDown += HandleKeyPressDown;
+            this.KeyUp += HandleKeyPressUp;
+            drawingPanel.MouseDown += HandleMousePressDown;
+            drawingPanel.MouseUp += CancelMousePressDown;
+        }
+
+        private void ConnectButton_Click(object sender, EventArgs e)
+        {
+            // Disable the form controls
+            ConnectButton.Enabled = false;
+            NameTextBox.Enabled = false;
+            IPAddressTextBox.Enabled = false;
+
+            // Enable the global form to capture key presses
+            KeyPreview = true;
+
+            controller.ConnectButton(IPAddressTextBox.Text, NameTextBox.Text);
+            this.Controls.Add(drawingPanel);
+            drawingPanel.MouseMove += HandleMouseMovements;
+        }
+
+        /// <summary>
+        /// Handler for the controller's UpdateArrived event
+        /// </summary>
+        private void OnFrame()
+        {
+            // Invalidate this form and all its children
+            // This will cause the form to redraw as soon as it can
+            MethodInvoker invalidator = new MethodInvoker(() => this.Invalidate(true));
+            controller.SetPlayerID(drawingPanel);
+            this.Invoke(invalidator);
+        }
+
+        /// <summary>
+        /// Key down handler
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void HandleKeyPressDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+                Application.Exit();
+            if (e.KeyCode == Keys.W || e.KeyCode == Keys.A || e.KeyCode == Keys.S || e.KeyCode == Keys.D)
+                controller.HandleMoveRequest(e);
+
+            // Prevent other key handlers from running
+            e.SuppressKeyPress = true;
+            e.Handled = true;
+        }
+
+        /// <summary>
+        /// Key down handler
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void HandleKeyPressUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+                Application.Exit();
+            if (e.KeyCode == Keys.W || e.KeyCode == Keys.A || e.KeyCode == Keys.S || e.KeyCode == Keys.D)
+                controller.CancelMoveRequest(e);
+        }
+
+        /// <summary>
+        /// Handle mouse down
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void HandleMousePressDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+                controller.HandleMouseRequest(e);
+            if (e.Button == MouseButtons.Right)
+                controller.HandleMouseRequest(e);
+        }
+
+        /// <summary>
+        /// Handle mouse down
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CancelMousePressDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+                controller.CancelMouseRequest(e);
+            if (e.Button == MouseButtons.Right)
+                controller.CancelMouseRequest(e);
+        }
+
+        /// <summary>
+        /// Handles mouse movements
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void HandleMouseMovements(object sender, MouseEventArgs e)
+        {
+            controller.MouseMoveRequest(e);
+        }
+    }
+}
